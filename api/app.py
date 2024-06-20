9d6f936f04befeb07b31fdd8bd0260314964714b
kalpit56 <kalpitmody@gmail.com>
2024-06-19 20:41:18 -0500

    removing uneeded cors as already in config.py

--- api/app.py
+++ api/app.py
@@ -9,21 +9,6 @@ from api.routers import (
 
 logger = logging.getLogger(__name__)
 
-origins = [
-    "http://135.148.121.103:4200",
-    "http://localhost:4200" 
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],  # You can restrict allowed methods if needed
-    allow_headers=["*"],  # You can restrict allowed headers if needed
-)
-
-
-
 app.include_router(v1.router)
 
 
