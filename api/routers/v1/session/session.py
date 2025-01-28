adf2d99096f76a30e5ade4eba84dd2a6d0c1ecd1
kalpit56 <kalpitmody@gmail.com>
2025-01-28 00:58:24 -0600

    trying to fix error

--- api/routers/v1/session/session.py
+++ api/routers/v1/session/session.py
@@ -251,7 +251,7 @@ async def recalculate_helper(pn_id: str) -> dict:
         date = session.get("date")
 
         total_sessions_played = total_sessions_played + 1
-        all_time_total = float(all_time_total) + winnings
+        all_time_total += winnings
         average_all_time_win_or_loss = all_time_total / total_sessions_played
 
         if (biggest_win < winnings):
@@ -270,8 +270,7 @@ async def recalculate_helper(pn_id: str) -> dict:
             negative_percentage = (number_of_sessions_negative / total_sessions_played) * 100
             positive_percentage = (number_of_sessions_positive / total_sessions_played) * 100
 
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    sql = update(Profile).values(last_updated_timestamp = current_time, all_time_total = all_time_total, biggest_win = biggest_win, biggest_loss = biggest_loss, date_of_biggest_win = date_of_biggest_win, date_of_biggest_loss = date_of_biggest_loss, average_all_time_win_or_loss = average_all_time_win_or_loss, positive_percentage = positive_percentage, negative_percentage = negative_percentage, number_of_sessions_positive = number_of_sessions_positive, number_of_sessions_negative = number_of_sessions_negative, total_sessions_played = total_sessions_played).where(Profile.pn_id == id)
+    sql = update(Profile).values(last_updated_timestamp = datetime.now(), all_time_total = all_time_total, biggest_win = biggest_win, biggest_loss = biggest_loss, date_of_biggest_win = date_of_biggest_win, date_of_biggest_loss = date_of_biggest_loss, average_all_time_win_or_loss = average_all_time_win_or_loss, positive_percentage = positive_percentage, negative_percentage = negative_percentage, number_of_sessions_positive = number_of_sessions_positive, number_of_sessions_negative = number_of_sessions_negative, total_sessions_played = total_sessions_played).where(Profile.pn_id == id)
     async with USERDATA_ENGINE.get_session() as session:
             session: AsyncSession = session
             async with session.begin():
