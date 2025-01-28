7163b3b059c18ad30469d43f6b406d812a01f210
kalpit56 <kalpitmody@gmail.com>
2025-01-28 00:47:43 -0600

    bug fix for recalc

--- api/routers/v1/session/session.py
+++ api/routers/v1/session/session.py
@@ -270,7 +270,8 @@ async def recalculate_helper(pn_id: str) -> dict:
             negative_percentage = (number_of_sessions_negative / total_sessions_played) * 100
             positive_percentage = (number_of_sessions_positive / total_sessions_played) * 100
 
-    sql = update(Profile).values(last_updated_timestamp = datetime.now(), all_time_total = all_time_total, biggest_win = biggest_win, biggest_loss = biggest_loss, date_of_biggest_win = date_of_biggest_win, date_of_biggest_loss = date_of_biggest_loss, average_all_time_win_or_loss = average_all_time_win_or_loss, positive_percentage = positive_percentage, negative_percentage = negative_percentage, number_of_sessions_positive = number_of_sessions_positive, number_of_sessions_negative = number_of_sessions_negative, total_sessions_played = total_sessions_played).where(Profile.pn_id == id)
+    current_time = datetime.now()
+    sql = update(Profile).values(last_updated_timestamp = current_time, all_time_total = all_time_total, biggest_win = biggest_win, biggest_loss = biggest_loss, date_of_biggest_win = date_of_biggest_win, date_of_biggest_loss = date_of_biggest_loss, average_all_time_win_or_loss = average_all_time_win_or_loss, positive_percentage = positive_percentage, negative_percentage = negative_percentage, number_of_sessions_positive = number_of_sessions_positive, number_of_sessions_negative = number_of_sessions_negative, total_sessions_played = total_sessions_played).where(Profile.pn_id == id)
     async with USERDATA_ENGINE.get_session() as session:
             session: AsyncSession = session
             async with session.begin():
